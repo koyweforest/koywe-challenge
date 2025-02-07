9e58f64a7d533637c1082450a1c79ee6a8be8bc7
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-07 11:50:01 -0300

    configuro modulo

--- src/currency/currency.module.ts
+++ src/currency/currency.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CurrencyController } from './currency.controller';
 import { CurrencyService } from './currency.service';
 
+// me aseguro que controlador y servicio esten registrados
 @Module({
   controllers: [CurrencyController],
   providers: [CurrencyService]
