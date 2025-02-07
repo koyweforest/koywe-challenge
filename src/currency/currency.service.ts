f57d12ddd41ce2214e055744d596be47f2755f22
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-07 12:56:49 -0300

    integro servicio en CurrencyService

--- src/currency/currency.service.ts
+++ src/currency/currency.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { CurrencyRateService } from './currency-rate.service';
+
 
 @Injectable()
 export class CurrencyService {
+    constructor(private readonly currencyRateService: CurrencyRateService) {}
+
     async convertToCrypto(amount: number, currency: string, crypto: string): Promise<number> {
-        const exchangeRate = await this.getExchangeRate(currency, crypto);
+        const exchangeRate = await this.currencyRateService.getExchangeRate(currency, crypto);
         return amount * exchangeRate;
     }
 
     async convertToFiat(amount: number, crypto: string, currency: string): Promise<number> {
-        const exchangeRate = await this.getExchangeRate(crypto, currency);
+        const exchangeRate = await this.currencyRateService.getExchangeRate(crypto, currency);
         return amount * exchangeRate;
     }
 
     async createQuote(amount: number, from: string, to: string): Promise<{ amount: number; from: string; to: string }> {
-        const exchangeRate = await this.getExchangeRate(from, to);
+        const exchangeRate = await this.currencyRateService.getExchangeRate(from, to);
         const convertedAmount = amount * exchangeRate;
 
         return {
@@ -21,10 +25,5 @@ export class CurrencyService {
             from,
             to,
         };
-    }
-
-    private async getExchangeRate(from: string, to: string): Promise<number> {
-        //tasa de cambio simulada
-        return 0.5;
     }
 }
