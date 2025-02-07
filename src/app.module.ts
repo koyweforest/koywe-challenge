84cd9ada27642169e5616ad23ce319934d3beab3
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-07 11:29:53 -0300

    creo module controller y service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CurrencyController } from './currency/currency.controller';
+import { CurrencyModule } from './currency/currency.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [CurrencyModule],
+  controllers: [AppController, CurrencyController],
   providers: [AppService],
 })
 export class AppModule {}
