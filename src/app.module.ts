bb4279521ca870ee1491d289be6f0f942f2d6a8d
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-07 12:03:50 -0300

    configuro puerto y corro app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CurrencyController } from './currency/currency.controller';
 import { CurrencyModule } from './currency/currency.module';
 
 // importo CurrencyModule
 @Module({
   imports: [CurrencyModule],
-  controllers: [AppController, CurrencyController],
-  providers: [AppService],
 })
 export class AppModule {}
