3b870d1a6b7bd9554b04a5bde383d7ae7d6110a4
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-07 12:48:29 -0300

    creo servicio para consultar la api

---
+++ src/currency/currency-rate.service.ts
@@ -0,0 +1,22 @@
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import axios from "axios";
+
+@Injectable()
+export class CurrencyRateService {
+    async getExchange(from: string, to: string): Promise<number> {
+        const url = `https://api.exchange.cryptomkt.com/api/3/public/price/rate?from=${to}&to=${from}`;
+
+        try {
+            const response = await axios.get(url);
+            const rate = response.data.rate;
+
+            if (!rate) {
+                throw new HttpException('Tipo de cambio no disponible', HttpStatus.NOT_FOUND);
+            }
+
+            return rate;
+        } catch (error) {
+            throw new HttpException('Error al obtener el tipo de cambio', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
