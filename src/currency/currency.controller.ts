1e1603342a01ff1aabc9a796f78a2d3f5d06fe55
pedro-donoso <pedro.donoso.r@usach.cl>
2025-02-07 11:40:19 -0300

    implemento controller

--- src/currency/currency.controller.ts
+++ src/currency/currency.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CurrencyService } from './currency.service';
 
-@Controller('currency')
-export class CurrencyController {}
+@Controller('api/currency')
+export class CurrencyController {
+  constructor(private readonly currencyService: CurrencyService) {}
+
+  @Post('convert-to-crypto')
+  async convertToCrypto(
+    @Body() body: { amount: number; currency: string; crypto: string },
+  ) {
+    return this.currencyService.convertToCrypto(
+      body.amount,
+      body.currency,
+      body.crypto,
+    );
+  }
+
+  @Post('convert-to-fiat')
+  async convertToFiat(
+    @Body() body: { amount: number; crypto: string; currency: string },
+  ) {
+    return this.currencyService.convertToFiat(
+      body.amount,
+      body.crypto,
+      body.currency,
+    );
+  }
+}
--- src/currency/currency.service.ts
+++ src/currency/currency.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CurrencyService {}
+export class CurrencyService {
+    convertToFiat(amount: number, crypto: string, currency: string) {
+        throw new Error('Method not implemented.');
+    }
+    convertToCrypto(amount: number, currency: string, crypto: string) {
+        throw new Error('Method not implemented.');
+    }
+}
